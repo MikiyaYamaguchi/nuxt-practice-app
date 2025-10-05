35052b4cdda11632f56304236e63b1660867045f
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-10-05 14:41:09 +0900

    app.vueにて、イベント処理について学ぶ

--- app/app.vue
+++ app/app.vue
@@ -1,38 +1,13 @@
 <script setup lang="ts">
-import { computed, onMounted, reactive } from "vue";
-
-const widthInit = Math.round(Math.random() * 10);
-const heightInit = Math.round(Math.random() * 10);
-
-const rectangle = reactive({
-  width: widthInit,
-  height: heightInit,
-});
-
-// const width = ref(widthInit);
-// const height = ref(heightInit);
-
-const area = computed((): number => {
-  // return width.value * height.value;
-  return rectangle.width * rectangle.height;
-});
-
-onMounted(() => {
-  setInterval((): void => {
-    // width.value = Math.round(Math.random() * 10);
-    // height.value = Math.round(Math.random() * 10);
-    rectangle.width = Math.round(Math.random() * 10);
-    rectangle.height = Math.round(Math.random() * 10);
-  }, 1000);
-});
+const msg = ref("まだ");
+const onButtonClick = (label: string, event: Event): void => {
+  const target = event.target as HTMLButtonElement;
+  const text = target.innerHTML;
+  msg.value = `${label}と${text}`;
+};
 </script>
 
 <template>
-  <div>
-    <p>
-      縦{{ rectangle.height }}で横が{{ rectangle.width }}の長方形の面積は{{
-        area
-      }}
-    </p>
-  </div>
+  <p>{{ msg }}</p>
+  <button v-on:click="onButtonClick('Hello', $event)">こんにちは</button>
 </template>
