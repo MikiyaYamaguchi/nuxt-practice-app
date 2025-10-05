3cf7a1fa70c08acfbcae01cba4d0afea6aec0791
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-10-05 15:00:44 +0900

    v-htmlでHTML文字列をエスケープさせずにレンダリングする手法を学習

--- app/app.vue
+++ app/app.vue
@@ -6,10 +6,12 @@ const msg = reactive({
 watch(msg, () => {
   msg.msg2 = msg.msg1;
 });
+const htmlCode = "<h2>見出し</h2><p>テキストが入ります。</p>";
 </script>
 
 <template>
   <p>{{ msg.msg1 }}</p>
   <p>ここにも表示されるよ：{{ msg.msg2 }}</p>
   <input type="text" v-model="msg.msg1" />
+  <section v-html="htmlCode"></section>
 </template>
