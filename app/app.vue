54c07a64211cc3a6a59e2c86d8811deec3aaad95
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-10-05 14:10:15 +0900

    app.vueでリアクティブ関数の学習

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,38 @@
+<script setup lang="ts">
+import { computed, onMounted, reactive } from "vue";
+
+const widthInit = Math.round(Math.random() * 10);
+const heightInit = Math.round(Math.random() * 10);
+
+const rectangle = reactive({
+  width: widthInit,
+  height: heightInit,
+});
+
+// const width = ref(widthInit);
+// const height = ref(heightInit);
+
+const area = computed((): number => {
+  // return width.value * height.value;
+  return rectangle.width * rectangle.height;
+});
+
+onMounted(() => {
+  setInterval((): void => {
+    // width.value = Math.round(Math.random() * 10);
+    // height.value = Math.round(Math.random() * 10);
+    rectangle.width = Math.round(Math.random() * 10);
+    rectangle.height = Math.round(Math.random() * 10);
+  }, 1000);
+});
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <p>
+      縦{{ rectangle.height }}で横が{{ rectangle.width }}の長方形の面積は{{
+        area
+      }}
+    </p>
   </div>
 </template>
