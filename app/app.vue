dd360bb97487ae601239bd0bcb88e2459482c5ba
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-10-05 15:05:57 +0900

    v-preでは以下のテンプレート記述をすべて無効化する学習

--- app/app.vue
+++ app/app.vue
@@ -7,6 +7,9 @@ watch(msg, () => {
   msg.msg2 = msg.msg1;
 });
 const htmlCode = "<h2>見出し</h2><p>テキストが入ります。</p>";
+const showHello = (): void => {
+  console.log("Hello");
+};
 </script>
 
 <template>
@@ -14,4 +17,7 @@ const htmlCode = "<h2>見出し</h2><p>テキストが入ります。</p>";
   <p>ここにも表示されるよ：{{ msg.msg2 }}</p>
   <input type="text" v-model="msg.msg1" />
   <section v-html="htmlCode"></section>
+  <section v-pre>
+    <p v-on:click="showHello">{{ msg.msg1 }}</p>
+  </section>
 </template>
