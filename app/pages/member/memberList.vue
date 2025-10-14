114245fcbea5982be4ea5866a30ba5dc6424a312
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-10-14 21:31:13 +0900

    ルーティングの制御（push関数で画面自動遷移など）について学習

--- app/pages/member/memberList.vue
+++ app/pages/member/memberList.vue
@@ -15,7 +15,11 @@ const memberList = useState<Map<number, Member>>("memberList");
   </nav>
   <section>
     <h2>会員リスト</h2>
-    <p>新規登録はこちらから</p>
+    <p>
+      新規登録は<NuxtLink v-bind:to="{ name: 'member-memberAdd' }"
+        >こちら</NuxtLink
+      >から
+    </p>
     <section>
       <ul>
         <li v-for="[id, member] in memberList" v-bind:key="id">
