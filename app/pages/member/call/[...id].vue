6b5b91029da1066096e52f6f234061f541798372
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-10-14 21:44:53 +0900

    可変長パラメータの設定について学習

---
+++ app/pages/member/call/[...id].vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>ルートパラメーターサンプル</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li><NuxtLink v-bind:to="{ name: 'index' }">TOP</NuxtLink></li>
+      <li>ルートパラメータ参照</li>
+    </ul>
+  </nav>
+  <section>
+    <p>{{ $route.params }}</p>
+  </section>
+</template>
