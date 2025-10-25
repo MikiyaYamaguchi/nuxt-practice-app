3ea96b0f581678ea7e9044c2b11d3b860478e33c
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-10-25 16:28:47 +0900

    レンダリング時のエラーを解消する手法を学習（natigateToで画面遷移）

---
+++ app/pages/errorHandlerNavigate.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const onResetButtonClick = async (errorProps: {
+  clearError: () => void;
+}): Promise<void> => {
+  await navigateTo("/");
+  errorProps.clearError();
+};
+</script>
+
+<template>
+  <NuxtErrorBoundary>
+    <ErrorGeneratorImmediate />
+    <template v-slot:error="errorProps">
+      <p>エラーが発生しました！</p>
+      <p>{{ errorProps.error }}</p>
+      <button @click="onResetButtonClick(errorProps)">エラーを解消</button>
+    </template>
+  </NuxtErrorBoundary>
+  <p>
+    <NuxtLink v-bind:to="{ name: 'index' }"> 戻る </NuxtLink>
+  </p>
+</template>
