866a3c96d27420839ab33b7aac62605f414d61a2
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-10-25 16:09:41 +0900

    エラーを解消された時の処理を実行する手法を学習

--- app/pages/errorHandlerBasic.vue
+++ app/pages/errorHandlerBasic.vue
@@ -1,12 +1,19 @@
+<script setup lang="ts">
+const onResetButtonClick = (errorProps: { clearError: () => void }) => {
+  errorProps.clearError();
+};
+</script>
+
 <template>
   <NuxtErrorBoundary>
     <ErrorGeneratorBasic />
     <template v-slot:error="errorProps">
-      <p>エラーが発生しました！</p>
+      <p>エラーが発生しました。</p>
       <p>{{ errorProps.error }}</p>
+      <button @click="onResetButtonClick(errorProps)">エラーを解消</button>
     </template>
   </NuxtErrorBoundary>
   <p>
-    <NuxtLink v-bind:to="{ name: 'index' }"> 戻る </NuxtLink>
+    <NuxtLink v-bind:to="{ name: 'index' }">戻る</NuxtLink>
   </p>
 </template>
